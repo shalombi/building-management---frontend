22a056c0865e597a9813cb91a0de5a1986d86b9b
Shalom biton <shalombiton555@gmail.com>
2023-06-27 21:20:18 +0300

    support add malfunction

--- src/cmps/edit-input.jsx
+++ src/cmps/edit-input.jsx
@@ -17,7 +17,7 @@ export const EditInput = ({ name, value, handleChange, title }) => {
                     onChange={handleChange}
                     id="name"
                     className="peer block w-full border-0 bg-gray-50 py-1.5 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="Jane Smith"
+                    placeholder="תיאור התקלה"
                 />
                 <div
                     className="absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600"
